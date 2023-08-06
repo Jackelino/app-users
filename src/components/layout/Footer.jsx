5395eb03460a3ac99631ba66827ab4b62521e620
JoseFernandez2020 <codigo.gratuito2019@gmail.com>
2023-08-05 21:00:23 -0600

    se crean los estilos del footer

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,7 +1,12 @@
+import styles from '../../assets/css/layout/Footer.module.css';
+const link = 'https://github.com/Jackelino/app-users';
 export function Footer() {
     return (
-        <footer>
-            soy el footer
+        <footer className={styles.footer}>
+            <small className={styles.copyRight}>
+                ©{new Date().getFullYear()} create by <b className={styles.name}>Jack </b>❤️ - All Rights Reserved.
+                <a href={link} target="_bank" className={styles.source}>Source 💻</a>
+            </small>
         </footer>
     )
 }
